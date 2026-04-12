a2c1992d636885710be02b36fcd5cc6a42ddd053
Alex Sushchenko <efremovdol.com>
2026-04-12 19:18:30 +0200

    build: add Coil dependency and implement product image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.coil.compose)
 
     // Hilt
     implementation(libs.hilt.android)
--- app/src/main/java/com/shashavs/fyultest/ui/components/ProductListItem.kt
+++ app/src/main/java/com/shashavs/fyultest/ui/components/ProductListItem.kt
@@ -1,8 +1,10 @@
 package com.shashavs.fyultest.ui.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
@@ -10,9 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.shashavs.fyultest.ui.models.ProductUI
 
 @Composable
@@ -22,17 +26,28 @@ fun ProductListItems(
 ) {
     ListItem(
         modifier = Modifier
-            .padding(horizontal = 24.dp, vertical = 4.dp)
+            .padding(horizontal = 12.dp, vertical = 4.dp)
             .fillMaxWidth()
             .clip(RoundedCornerShape(8.dp))
             .clickable { onClick() },
+
         leadingContent = {
-            // TODO impl load product thumbnail
+            AsyncImage(
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(width = 1.dp, color = MaterialTheme.colorScheme.surfaceVariant, shape = RoundedCornerShape(8.dp)),
+                model = product.thumbnail,
+                placeholder = ColorPainter(MaterialTheme.colorScheme.surfaceVariant),
+                contentDescription = product.title,
+            )
         },
         headlineContent = {
             Text(
                 text = product.title,
-                style = MaterialTheme.typography.titleMedium
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
         },
         supportingContent = {
@@ -53,7 +68,8 @@ fun PreviewProductListItems() {
         product = ProductUI(
             id = 1,
             title = "Product Title",
-            description = "Product Description",
+            description = "Product Description Product Description Product Description " +
+                    "Product Description Product Description",
             thumbnail = ""
         )
     )

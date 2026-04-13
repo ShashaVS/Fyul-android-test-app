e7cbd65591b5dc27d96579417b665809de1c9fff
Alex Sushchenko <efremovdol.com>
2026-04-13 10:52:40 +0200

    ui: add TopAppBar to MainActivity

--- app/src/main/java/com/shashavs/fyultest/MainActivity.kt
+++ app/src/main/java/com/shashavs/fyultest/MainActivity.kt
@@ -6,14 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.shashavs.fyultest.ui.screens.ProductListScreen
 import com.shashavs.fyultest.ui.theme.FYULTestTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +28,13 @@ class MainActivity : ComponentActivity() {
             FYULTestTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(stringResource(R.string.app_name))
+                            }
+                        )
+                    },
                     content = { innerPadding ->
                         ProductListScreen(
                             modifier = Modifier.padding(innerPadding)
